9340ffe3f622159af13d0d370c1b5cf3fba56ae8
忆人已醉 <2209948918@qq.com>
2026-06-08 20:47:40 +0800

    Build only Android Rust library

--- flutter/ndk_arm64.sh
+++ flutter/ndk_arm64.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 ANDROID_API_LEVEL="${ANDROID_API_LEVEL:-21}"
 CARGO_FEATURES="${CARGO_FEATURES:-flutter,hwcodec}"
 
-cargo ndk --platform "${ANDROID_API_LEVEL}" --target aarch64-linux-android build --release --features "${CARGO_FEATURES}"
+cargo ndk --platform "${ANDROID_API_LEVEL}" --target aarch64-linux-android build --release --lib --features "${CARGO_FEATURES}"
