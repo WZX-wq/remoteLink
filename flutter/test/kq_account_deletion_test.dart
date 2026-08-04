a21be60d2617f8f30f9818bb34be819de2fe78f0
m4_txx <m4_txx@m4-txxdeMac-mini.local>
2026-08-04 11:11:02 +0800

    fix(ios): clarify account deletion data warning

--- flutter/test/kq_account_deletion_test.dart
+++ flutter/test/kq_account_deletion_test.dart
@@ -124,11 +124,11 @@ void main() {
     expect(source, contains('Delete account'));
   });
 
-  test('account deletion warns that membership data will be removed', () {
+  test('account deletion warns that all Kunqiong account data will be removed', () {
     final page =
         File('lib/mobile/pages/account_deletion_page.dart').readAsStringSync();
 
-    expect(page, contains('注销账号会清理掉鲲穹账户的所有会员数据，请您谨慎注销。'));
+    expect(page, contains('注销账号会清理鲲穹账户的所有数据，请您谨慎注销。'));
   });
 
   test('successful deletion clears all account-scoped local data', () {
